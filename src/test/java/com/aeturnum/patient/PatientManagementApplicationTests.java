96b98ec3d76e15e035279d734cb44e35169fd6ab
Nibras <mohamednibras111@gmail.com>
2022-06-08 13:08:30 +0530

    Initial commit

---
+++ src/test/java/com/aeturnum/patient/PatientManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeturnum.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
